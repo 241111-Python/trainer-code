43e141837151970ee0858ee1fec3c67cf8935ed2
Richard Hawkins <richard.hawkins@revature.com>
2024-11-14 10:22:46 -0500

    pi example and scripting parameter example

--- Week1/script_source.sh
+++ Week1/script_source.sh
@@ -1,6 +1,12 @@
-hello_world() {
-	echo "Hello World"
-}
+hello_world() {						# start a function called hello_world into memory
+	echo "Hello World"				# content of the function
+}									# this is the end of the hello_world function
 
-"$@"
+function main(){					# Start a function called main in memory
+	echo "From main(): $@"			# When main runs, take the first argument it was passed, and echo it out.
+	echo "From main, count of parameters: $#"
+}	
 
+echo "From the script file: $1"		# this argument is coming from the CLI arguments
+echo "From the script file: $@"
+main $@
