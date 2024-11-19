a5f815350167615003ed6771c83c7fd5582d59e1
Richard Hawkins <Richard.Hawkins@Revature.com>
2024-11-19 09:19:28 -0600

    add python exercises

---
+++ Week2/python-exercises/string_wrap.py
@@ -0,0 +1,33 @@
+import unittest
+
+# Write a method that takes in a starting character and the number of characters to return,
+# and returns a string of letters from the alphabet.
+# If a string would extend pass "z", it should "wrap around" back to "a" and continue from the beginning again.
+# If the starting letter is lower case, the string should be lowercase.
+# If the starting letter is upper case, the string should be uppercase.
+
+def wrap_around(s, n):
+    pass
+
+########################### TESTS ##############################################################
+class TestMethods(unittest.TestCase):
+
+    def test_string_wrap_1(self):
+        result = wrap_around('a', 5)
+        self.assertEquals(result, 'abcde')
+        
+    def test_string_wrap_2(self):
+        result = wrap_around('x', 6)
+        self.assertEquals(result, 'xyzabc')
+    
+    def test_string_wrap_3(self):
+        result = wrap_around('G', 10)
+        self.assertEquals(result, 'GHIJKLMNOP')
+    
+    def test_string_wrap_4(self):
+        result = wrap_around('A', 60)
+        self.assertEquals(result, 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGH')
+    
+
+if __name__ == '__main__':
+    unittest.main()
