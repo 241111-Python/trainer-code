49872a6e352cf3691d4e02a931df394b24ed3f59
Richard Hawkins <Richard.Hawkins@Revature.com>
2024-12-04 14:18:28 -0500

    add matplotlib examples

--- Week4/PandasDemo/pd_csv.py
+++ Week4/PandasDemo/pd_csv.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import csv
+import matplotlib.pyplot as plt
 
 path = './../../SourceData/pokemon.csv'
 
@@ -9,6 +10,22 @@ path = './../../SourceData/pokemon.csv'
 #     for row in reader:
 #         print(*row)
 
-df = pd.read_csv(path, index_col='ID', usecols=['ID', 'Name', 'Type 1', 'Type 2', 'HP', 'Generation'])
+df = pd.read_csv(path, index_col='ID', usecols=['ID', 'Total', 'HP', 'Attack', 'Defense', 'Speed'])
 # print(df[['Name','Generation']])
+
+
+# Histogram
+#plt.hist(df['HP'])
+#plt.show()
+#plt.savefig("HP_Histogram.png")
+
+# Scatter plotting
+# plt.scatter(df['Attack'], df['Defense'])
+# plt.savefig("attack_defense.png")
+
+
+df.sort_values('HP', inplace=True)
 print(df)
+plt.plot(df['HP'], df['Speed'])
+plt.plot(df['HP'], df['Defense'])
+plt.savefig("HP-Speed-Line.png")
